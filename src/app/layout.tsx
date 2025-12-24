83f31e69e67b7c68e0ad26b8583b3c09ed0488fe
collins vanziacp <collinsvanziacp@gmail.com>
2025-12-24 14:32:06 +0000

    Remove this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { AppScripts } from './app-scripts';
 import Header from '@/components/header';
 
 
@@ -53,7 +52,6 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
         </div>
         <Toaster />
-        <AppScripts />
       </body>
     </html>
   );
