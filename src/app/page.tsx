d0f2c9e631133c3b175575835f15ebecc471aedd
collins vanziacp <collinsvanziacp@gmail.com>
2025-12-25 10:58:40 +0000

    Undo last 2 changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,6 +61,7 @@ export default function BotBuilderPage() {
 
                 <TabsContent value="dcircle">
                     <DigitAnalysisTool />
+                    <QuickTradePanel />
                 </TabsContent>
 
                 <TabsContent value="trading-view" forceMount>
