702051142a5425ef06ef7e34f0a41dbeb221ba77
collins vanziacp <collinsvanziacp@gmail.com>
2025-12-25 10:56:49 +0000

    Remove the tradingview thing on botbuilder and and dcircle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,6 @@ export default function BotBuilderPage() {
 
                 <TabsContent value="dcircle">
                     <DigitAnalysisTool />
-                    <QuickTradePanel />
                 </TabsContent>
 
                 <TabsContent value="trading-view" forceMount>
